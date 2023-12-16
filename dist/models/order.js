8786d26d5169509de3c303d3dfc1f700f6903c63
ikotun-dev <danlogan2003@gmail.com>
2023-12-16 13:44:28 +0100

    added Order model

---
+++ dist/models/order.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const orderSchema = new mongoose_1.Schema({
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        required: true
+    },
+    products: {
+        type: [mongoose_1.Schema.Types.ObjectId],
+        ref: 'Seller',
+        required: true
+    },
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+    customer: {
+        type: mongoose_1.Schema.Types.ObjectId,
+        ref: 'Customer',
+        required: true
+    }
+});
+const orderModel = (0, mongoose_1.model)('Order', orderSchema);
+exports.default = orderModel;
