36da18fb22b54922fea1f2fbd0fb02876ed27901
ikotun-dev <danlogan2003@gmail.com>
2024-07-27 14:18:56 +0100

    fixes

--- models/order.ts
+++ models/order.ts
@@ -1,36 +1,35 @@
-import { Schema, Document, model, Types } from 'mongoose'
+import { Schema, Document, model, Types } from "mongoose";
+//test
 
-interface OrderInterface extends Document { 
-    createdAt : Date,
-    products : Types.ObjectId[],
-    totalPrice : number,
-    customer : Types.ObjectId
+interface OrderInterface extends Document {
+  createdAt: Date;
+  products: Types.ObjectId[];
+  totalPrice: number;
+  customer: Types.ObjectId;
 }
 
-const orderSchema = new Schema<OrderInterface> ({
-    createdAt : {
-        type : Date,
-        default : Date.now(),
-        required : true
-    },
-    products : {
-        type : [Schema.Types.ObjectId],
-        ref : 'Product',
-        required : true
-    },
-    totalPrice : {
-        type : Number,
-        required : true 
-    },
-    customer : {
-        type : Schema.Types.ObjectId,
-        ref : 'Customer',
-        required: true
-    }
+const orderSchema = new Schema<OrderInterface>({
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+  products: {
+    type: [Schema.Types.ObjectId],
+    ref: "Product",
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  customer: {
+    type: Schema.Types.ObjectId,
+    ref: "Customer",
+    required: true,
+  },
+});
 
-})
-
-const orderModel = model<OrderInterface>('Order', orderSchema)
+const orderModel = model<OrderInterface>("Order", orderSchema);
 
 export default orderModel;
-
